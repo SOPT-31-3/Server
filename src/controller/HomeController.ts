c1f8ca756528e2aa256380b4e0f6157dd4090baa
ehdwoKIM <kinbell19@gmail.com>
2022-11-24 19:16:16 +0900

    [FIX] 커밋 수정

--- src/controller/HomeController.ts
+++ src/controller/HomeController.ts
@@ -1,21 +1,19 @@
+import { Request, Response } from "express";
+import { homeService } from "../service";
 
-// /**
-//  *
-//  * @routes GET /api/home
-//  * @desc 홈 화면 조회
-//  * @access public
-//  */
-//  const getHome = async (req: Request, res: Response) => {
-//     const { tripId } = req.params;
-    
-//     const data = await HomeService.getSeries(seriesId);
-    
-//     if (!data) {
-//         return res.status(404).json({status: 404, message: "NOT_FOUND"});
-//     }
-//     return res.status(200).json({status: 200, message: "회차 정보 조회 성공", data});
-// };
-  
-// export default {
-//     getHome
-// };
+const getHomeById = async (req: Request, res: Response) => {
+  const { tripId } = req.params;
+
+  const data = await homeService.getHomeById(+tripId);
+
+  if (!data) {
+    return res.status(404).json({ status: 404, message: "NOT_FOUND" });
+  }
+  return res.status(200).json({ status: 200, message: "홈 화면 조회 성공", data });
+};
+
+const homeController = {
+    getHomeById,
+}; 
+
+export default homeController;
