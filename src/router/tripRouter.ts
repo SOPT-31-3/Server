47e892cda78340b314e4dfb411ed01a052b39b0c
ehdwoKIM <kinbell19@gmail.com>
2022-11-30 12:39:38 +0900

    [FIX] 전체 일정 저장 코드 수정

--- src/router/tripRouter.ts
+++ src/router/tripRouter.ts
@@ -8,6 +8,6 @@ router.post("/", tripController.createTrip);
 //* 도시 선택
 router.put("/:tripId/city", tripController.updateCity);
 //* 전체 일정 저장
-router.post("/plan/allPlan", tripController.saveAllPlan);
+router.post("/plan/allPlan/", tripController.saveAllPlan);
 
 export default router;
